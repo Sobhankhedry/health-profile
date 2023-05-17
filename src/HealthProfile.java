c891585e72de784fe51604b3c4f770ba196e20bc
Sobh6n <sobhankhedry7297@gmail.com>
2023-05-17 18:44:35 +0330

    some attributes and setters and getters were added

--- src/HealthProfile.java
+++ src/HealthProfile.java
@@ -1,2 +1,67 @@
 public class HealthProfile {
+    private String firstName, lastName;
+    private int age;
+    private int height;
+    private int weight;
+    private String dateOfBirth, gender;
+
+    public HealthProfile(String firstName, String lastName){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
